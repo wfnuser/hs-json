4951dc18791ae8a7c0eb993195a2f2534b0bc9d4
wfnuser <wfnuser@126.com>
2021-04-29 00:12:34 +0800

    add char parser

--- Main.hs
+++ Main.hs
@@ -1,14 +1,21 @@
+{-# LANGUAGE LambdaCase #-}
 module Main where
 
 newtype Parser a = Parser {runParser :: String -> Maybe (String, a)}
 
-data JsonValue = JsonNull 
-    | JsonBool Bool 
-    deriving (Show, Eq)
+data JsonValue
+  = JsonNull
+  | JsonBool Bool
+  deriving (Show, Eq)
 
 jsonValue :: Parser JsonValue
 jsonValue = undefined
 
+charP :: Char -> Parser Char
+charP x = Parser $ \case
+    y : ys
+      | y == x -> Just (ys, x)
+    _ -> Nothing
 
 main :: IO ()
 main = undefined
