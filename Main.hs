970034f1e239e6cb55ec559a35293ad13dc73637
wfnuser <wfnuser@126.com>
2021-04-29 13:22:40 +0800

    feat: support jsonNull parser

--- Main.hs
+++ Main.hs
@@ -23,20 +23,33 @@ stringP :: String -> Parser String
 stringP [] = Parser $ const Nothing
 stringP (x : xs) = Parser $ \case
   y : ys
-    | y == x -> (\case
-        Nothing -> Just (ys, [x]) 
-        Just (s, a) -> Just (s, x:a)
-    ) (runParser (stringP xs) ys)
+    | y == x ->
+      ( \case
+          Nothing -> Just (ys, [x])
+          Just (s, a) -> Just (s, x : a)
+      )
+        (runParser (stringP xs) ys)
     | otherwise -> Nothing
   _ -> Nothing
 
+-- we can use fmap to do that;
+-- just map string "NULL" to JsonNull
+-- jsonNull :: Parser JsonValue
+-- jsonNull = Parser $ \input -> (\case
+--                 Just (s, a) -> Just (s, JsonNull)
+--                 Nothing -> Nothing
+--             ) (runParser (stringP "NULL") input)
+
+-- first of all; we need to prove that Parser is a Functor
+
+instance Functor Parser where
+  fmap f (Parser p) = Parser $ \input ->
+    case p input of
+      Just (s, a) -> Just (s, f a)
+      Nothing -> Nothing
 
 jsonNull :: Parser JsonValue
-jsonNull = Parser $ \input -> (\case
-                Just (s, a) -> Just (s, JsonNull)
-                Nothing -> Nothing 
-            ) (runParser (stringP "NULL") input)
-
+jsonNull = fmap (const JsonNull) (stringP "NULL")
 
 main :: IO ()
 main = undefined
