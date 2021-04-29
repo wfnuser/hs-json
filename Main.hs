7869d072fc047d87fee5f251046165e2e473cf58
wfnuser <wfnuser@126.com>
2021-04-29 11:33:28 +0800

    string Parser

--- Main.hs
+++ Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE LambdaCase #-}
+
 module Main where
 
 newtype Parser a = Parser {runParser :: String -> Maybe (String, a)}
@@ -13,9 +14,22 @@ jsonValue = undefined
 
 charP :: Char -> Parser Char
 charP x = Parser $ \case
-    y : ys
-      | y == x -> Just (ys, x)
-    _ -> Nothing
+  y : ys
+    | y == x -> Just (ys, x)
+  _ -> Nothing
+
+stringP :: String -> Parser String
+stringP [] = Parser $ const Nothing
+stringP (x : xs) = Parser $ \case
+  y : ys
+    | y == x -> (\case
+        Nothing -> Just (ys, [x]) 
+        Just (s, a) -> Just (s, x:a)
+    ) (runParser (stringP xs) ys)
+    | otherwise -> Nothing
+  _ -> Nothing
+
+
 
 main :: IO ()
 main = undefined
